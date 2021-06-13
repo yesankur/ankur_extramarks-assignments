513181fb8cb2a4f6e17ceef124a58e5bc8b30f53
yesankur <80883671+yesankur@users.noreply.github.com>
2021-06-13 10:33:22 +0530

    Add files via upload

---
+++ javacomparator.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Checker implements Comparator<Player>{
+    public int compare (Player x1,Player x2){
+        if(x1.score==x2.score){
+            return x1.name.compareTo(x2.name);
+        }
+        else{
+            return x2.score-x1.score;
+        }
+    }
+}
+
+class Player{
+    String name;
+    int score;
+    
+    Player(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+}
+
+class javacomparator {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+        
+        for(int i = 0; i < n; i++){
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+
+        Arrays.sort(player, checker);
+        for(int i = 0; i < player.length; i++){
+            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        }
+    }
+}
